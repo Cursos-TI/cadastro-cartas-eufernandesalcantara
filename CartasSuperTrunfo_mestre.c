304e59e30f0a862df896e0a072a21c76afa46129
Felipe Fernandes Alcantara <eufernandesalcantara@gmail.com>
2025-09-25 02:17:25 +0000

    add

--- CartasSuperTrunfo_mestre.c
+++ CartasSuperTrunfo_mestre.c
@@ -167,8 +167,8 @@ int venceu_super_poder = super_poder1 > super_poder2;
   printf("PIB: %.2f bilhões de reais\n", pib2); // mostra o PIB 2 
   printf("Número de Pontos Turísticos: %d\n ", pontos_turisticos2); // mostra a quantidade de pontos turisticos 2
    // exibição dos novos dados calculados nivel aventureiro
-  printf("Densidade Populaciona2: %.2f hab/km²\n", densidade_populacional1);
-  printf("PIB per Capita: %.2f reais\n", pib_per_capita1);
+  printf("Densidade Populaciona2: %.2f hab/km²\n", densidade_populacional2);
+  printf("PIB per Capita: %.2f reais\n", pib_per_capita2);
   printf("Super Poder:%.2f\n", super_poder2);
 
 // Confronto final
